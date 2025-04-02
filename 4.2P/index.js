3cc153bbd0b9656277693e24ee9b3775f5a65b17
tran-haison <haison250199@gmail.com>
2025-04-03 04:47:35 +1100

    feat: work on task 4.2P - Part 2

--- 4.2P/index.js
+++ 4.2P/index.js
@@ -1,11 +1,29 @@
 var express = require("express")
+var mongoose = require("mongoose")
 var app = express()
+var port = process.env.port || 3000;
 
+// Middleware
 app.use(express.static(__dirname + '/public'))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-var port = process.env.port || 3000;
+mongoose.connect('mongodb://localhost:27017/myprojectDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB!');
+});
+
+const ProjectSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    link: String,
+    description: String,
+});
+const Project = mongoose.model('Project', ProjectSchema);
 
 const cardList = [
     {
@@ -21,10 +39,10 @@ const cardList = [
         desciption: "Demo desciption about kitten 3"
     }
 ]
-app.get('/api/projects', (req, res) => {
-    res.json({ statusCode: 200, data: cardList, message: "Success" })
+app.get('/api/projects', async (req, res) => {
+    const projects = await Project.find({});
+    res.json({ statusCode: 200, data: projects, message: "Success" })
 });
-
 
 app.listen(port, () => {
     console.log("App listening to: " + port);
