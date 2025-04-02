32e7a6d1c0a0bcfb77a3e70761079f071113213b
tran-haison <haison250199@gmail.com>
2025-04-03 04:09:37 +1100

    feat: work on task 4.2P - Part 1

---
+++ 4.2P/index.js
@@ -0,0 +1,35 @@
+var express = require("express")
+var app = express()
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+var port = process.env.port || 3000;
+
+const cardList = [
+    {
+        title: "Kitten 2",
+        image: "images/kitten-2.jpeg",
+        link: "About Kitten 2",
+        desciption: "Demo desciption about kitten 2"
+    },
+    {
+        title: "Kitten 3",
+        image: "images/kitten-3.jpg",
+        link: "About Kitten 3",
+        desciption: "Demo desciption about kitten 3"
+    }
+]
+app.get('/api/projects', (req, res) => {
+    res.json({ statusCode: 200, data: cardList, message: "Success" })
+});
+
+
+app.listen(port, () => {
+    console.log("App listening to: " + port);
+    console.log("http://localhost:" + port);
+
+    // Open index.html in the browser
+
+})
